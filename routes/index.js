6066618951ca35666ae904383efbef4fec46bdee
Jared Anthony Kronyak <jared.kronyak@gmail.com>
2022-06-12 13:59:12 -0400

    added: basic education and experience pages

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,14 @@ const constructorMethod = (app) => {
 		res.render('home');
 	});
 
+	app.get('/education', (_, res) => {
+		res.render('education');
+	});
+
+	app.get('/experience', (_, res) => { 
+		res.render('experience');
+	});
+
 	app.use('*', (_, res) => {
 		res.sendStatus(404);
 	});
