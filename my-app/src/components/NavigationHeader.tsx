9755b4fac5e264287478ef9beb18ab063e4532fd
jkronyak <56709613+jkronyak@users.noreply.github.com>
2023-08-21 20:50:39 -0400

    more styling adjustments

--- my-app/src/components/NavigationHeader.tsx
+++ my-app/src/components/NavigationHeader.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 import '../NavigationHeader.css';
 
 const NavigationHeader = () => {
+	const { pathname } = useLocation();
+	console.log(pathname);
     return (
         <div className='nav-header-div'>
             <nav>
-                <Link to="/">Home</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/experience">Experience</Link>
-                <Link to="/courses">Courses</Link>
+                <Link className={pathname === "/" ? 'active' : 'inactive'} to="/">Home</Link>
+                <Link className={pathname === "/projects" ? 'active' : 'inactive'} to="/projects">Projects</Link>
+                <Link className={pathname === "/experience" ? 'active' : 'inactive'} to="/experience">Experience</Link>
+                <Link className={pathname === "/courses" ? 'active' : 'inactive'} to="/courses">Courses</Link>
             </nav>
 			<hr></hr>
         </div>
