7bce0b1c07619249ab988e62ec945f5c123d90be
jkronyak <56709613+jkronyak@users.noreply.github.com>
2023-08-23 18:14:29 -0400

    More css adjustments to make footer fixed size

--- my-app/src/components/Footer.tsx
+++ my-app/src/components/Footer.tsx
@@ -7,9 +7,11 @@ const Footer = () => {
 		<div className='footer-div'>
 			<hr></hr>
 			<a href='https://github.com/jkronyak'>
+				<span className='hidden-span'>GitHub</span>
 				<img src="./img/github-mark.png"/>
 			</a>
 			<a href='https://www.linkedin.com/in/jared-kronyak/'>
+				<span className='hidden-span'>LinkedIn</span>
 				<img src="./img/linkedin-logo.png"/>
 			</a>
 		</div>
