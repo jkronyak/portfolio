2951c2ab403ca276d4151ec1fa451bc601a1ef0c
jkronyak <56709613+jkronyak@users.noreply.github.com>
2023-09-27 20:27:34 -0400

    updated resume link

--- my-app/src/pages/About/About.tsx
+++ my-app/src/pages/About/About.tsx
@@ -4,7 +4,7 @@ const About = () => {
 	return(
 		<div className="about-div">
 			<h2>My Resume</h2>
-			<embed src="https://drive.google.com/file/d/18Q8BG6iJSyQG8VNkCZ8TJk2-jZReUmY6/preview" width="640" height="480"/>
+			<embed src="https://drive.google.com/file/d/1XjCGPQmWhZuJEoAc1cFj5SVMZIUXkWCM/preview" width="640" height="480"/>
 		</div>
 	)
 }
